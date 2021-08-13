b8307f43d5acc2b471375aedfdea30e235f3722c
Guilherme Balzana <gb637@cam.ac.uk>
2021-08-13 14:17:56 +0100

    Remove python shebang and trailing space

--- astroFIRE/FUVfunctions.py
+++ astroFIRE/FUVfunctions.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Wed Jul 14 14:26:35 2021
 
@@ -7,7 +5,7 @@ Created on Wed Jul 14 14:26:35 2021
 """
 import numpy as np
 import matplotlib as mpl
-from matplotlib.colors import LinearSegmentedColormap 
+from matplotlib.colors import LinearSegmentedColormap
 from astropy.table import Table
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
@@ -15,7 +13,7 @@ from astropy import units as u
 
 
 
- # define the color-table and a normalization used for FUV           
+ # define the color-table and a normalization used for FUV
 cmapFUV = LinearSegmentedColormap.from_list('mycmap', [
                                                       (0.0, 'mediumpurple'),
                                                       (0.2, 'rebeccapurple'),
@@ -23,37 +21,37 @@ cmapFUV = LinearSegmentedColormap.from_list('mycmap', [
                                                       (0.58, 'gold'),
                                                       (0.75, 'orange'),
                                                       (0.8, 'tab:red'),
-                                                      (1.0, 'maroon')])  
-normFUV = mpl.colors.Normalize(vmin=np.log10(1.7), vmax=5) 
+                                                      (1.0, 'maroon')])
+normFUV = mpl.colors.Normalize(vmin=np.log10(1.7), vmax=5)
 
 def plotCmapFUV(**kargs):
     ax = None
     fig = None
     fontsize = None
-    if 'fig' in kargs.keys(): 
+    if 'fig' in kargs.keys():
         fig = kargs['fig']
     if 'ax' not in kargs.keys():
         fig, ax = plt.subplots(figsize=(0.5, 3), dpi=100)
         plt.subplots_adjust(left=0.15, right=0.95, bottom=0.12, top=0.97)
     else:
-        ax = kargs['ax']        
-    if 'fontsize' in kargs.keys():  
+        ax = kargs['ax']
+    if 'fontsize' in kargs.keys():
         fontsize = kargs['fontsize']
     else:
         fontsize = 14
-    cb1 = mpl.colorbar.ColorbarBase(ax, cmap=cmapFUV, norm=normFUV, 
-                                    orientation='vertical')        
-    cb1.ax.set_ylabel(r'log(F$_\mathrm{FUV}$) - (log(G$_0$)', rotation=270, 
+    cb1 = mpl.colorbar.ColorbarBase(ax, cmap=cmapFUV, norm=normFUV,
+                                    orientation='vertical')
+    cb1.ax.set_ylabel(r'log(F$_\mathrm{FUV}$) - (log(G$_0$)', rotation=270,
                       labelpad=20, fontsize=fontsize)
-         
+
 class DiskWithFUV:
     """
     Interpolates timescales for disk dissipation as a function of the local
     FUV flux, base on the results of the disk-dissipation model by Winter et
     al. (2020) MNRAS 491 90
     https://ui.adsabs.harvard.edu/abs/2020MNRAS.491..903W/abstract
-    
-    WKC20 grid provides: 
+
+    WKC20 grid provides:
         - tauvisc_Myr
         - Mstar_Msol
         - FUV_G0
@@ -61,59 +59,59 @@ class DiskWithFUV:
         - Rscale_au
         - alphavisc
         - Tdisp_Myr
-    
+
     -----
     Version: 14th July 2021
-    
-    
-    
-                - input - 
+
+
+
+                - input -
     mass: in Msun
     FUV:  in Go
     tau_vis=1. is the recommended value by Andrew Winter
     curves: default=False
     ---
-    - output- 
-    if curves=False, returns tau_d which is the timescale for 
+    - output-
+    if curves=False, returns tau_d which is the timescale for
         disk-dissipation by external photoevaporation under FUV
     if curves=True, returns a curve of FUV,tau_d for the mass given
-         
+
     """
     def __init__(self):
         """
         Load tabulations for a fixed viscous timescales as a grid
-        
-        --- 
+
+        ---
         Usage:
-            
+
             disk_model=DiskWithFUV()
 
         """
         import os
         datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                           'data/disk_model/WKC20/')     
-        self.WKC20 = Table.read(datadir + 
+                           'data/disk_model/WKC20/')
+        self.WKC20 = Table.read(datadir +
                                 'FUVdisp_R40_tauvisc.csv', format='csv')
         #masses for  which disk-models are calculated
-        self.M = np.array([0.1, 0.3, 0.5, 0.8, 1., 1.3, 1.6, 1.9]) 
+        self.M = np.array([0.1, 0.3, 0.5, 0.8, 1., 1.3, 1.6, 1.9])
         self.FUV = np.unique(np.array(self.WKC20['FUV_G0']))
     def get_tauD_vs_FUV(self, mass, tau_vis=1.):
         """
-        Select within the grid the tabulations for a mass and and 
+        Select within the grid the tabulations for a mass and and
         viscous timescale.
-        
-        Note that WKC20 models are estimated such as under no FUV influence, 
+
+        Note that WKC20 models are estimated such as under no FUV influence,
         disks are dissipated in 10 Myr due to internal processes. Hence
-        I am appending tau_D=10 Myr at FUV=0 to these arrays. 
-        
+        I am appending tau_D=10 Myr at FUV=0 to these arrays.
+
         ____
         input:
-            mass = mass in Msun 
+            mass = mass in Msun
             tau_vis [1.0,2.0,5.0] = viscous timescale in Myrs
-        
+
         """
         self.mass = mass #needs to keep track to know where I am in the grid
-        select_tau_vis = self.WKC20['# tauvisc_Myr'] == tau_vis   
+        select_tau_vis = self.WKC20['# tauvisc_Myr'] == tau_vis
         if mass in self.M: #just select table for the M and interpolate values
             select_mass = self.WKC20['Mstar_Msol'] == mass
             self.tauD_Myr = 1e6*np.array(self.WKC20['Tdisp_Myr'][select_mass \
@@ -129,25 +127,25 @@ class DiskWithFUV:
                 x = [self.M[j], self.M[j + 1]]
             self.tauD_Myr = \
                np.array([1e6*np.interp(mass, x, y) for y in list(zip(\
-                               self.WKC20['Tdisp_Myr'][select_tau_vis & 
-                               (self.WKC20['Mstar_Msol'] == x[0])],  
-                               self.WKC20['Tdisp_Myr'][select_tau_vis & 
+                               self.WKC20['Tdisp_Myr'][select_tau_vis &
+                               (self.WKC20['Mstar_Msol'] == x[0])],
+                               self.WKC20['Tdisp_Myr'][select_tau_vis &
                                (self.WKC20['Mstar_Msol'] ==  x[1])]))])
-        
+
     def get_tauD(self, mass, FUV, tau_vis=1.):
         self.get_tauD_vs_FUV(mass, tau_vis=tau_vis)
         tauD_ = np.insert(self.tauD_Myr, 0, 10e6, axis=0)
         FUV_ = np.insert(self.FUV, 0, 1, axis=0)
-        if FUV in self.FUV: 
+        if FUV in self.FUV:
             return self.tauD_Myr[self.FUV == FUV][0]
         elif FUV > 1e4:
             return self.tauD_Myr[self.FUV == 1e4][0]
         elif FUV > 1:
-            #note that 
-            tauD = 10**interp1d(np.log10(FUV_), np.log10(tauD_), 
-                                kind='linear', bounds_error=False, 
+            #note that
+            tauD = 10**interp1d(np.log10(FUV_), np.log10(tauD_),
+                                kind='linear', bounds_error=False,
                                 fill_value='extrapolate')(np.log10(FUV))
-        elif FUV > 0: 
+        elif FUV > 0:
             tauD = 10e6
         elif FUV < 0:
             print("I can't be negative!")
@@ -157,11 +155,11 @@ class DiskWithFUV:
         return tauD
 class Parravano:
     """
-    Based on Parravano et al. (2003) ApJ 584 797 [1], this class provides 
+    Based on Parravano et al. (2003) ApJ 584 797 [1], this class provides
     the tools to estimate the amount of far-ultraviolet radiation outputed
     by massive stars. The scalling relations used here are defined in their
     Table 1 and are available for stars in the mass range 1.8-120Msun
-    
+
     [1] https://ui.adsabs.harvard.edu/abs/2003ApJ...584..797P/abstract
     """
     def __init__(self, mass):
@@ -171,9 +169,9 @@ class Parravano:
         self.G0 = u.def_unit(r'G_0', 1.6e-3 << u.erg/(u.cm**2)/u.s )
     def LEUV(self):
         """
-        Gives extreme-ultraviolet (h\nu > 13.6eV) luminosities as a function 
+        Gives extreme-ultraviolet (h\nu > 13.6eV) luminosities as a function
         of stellar mass.
-        
+
         ----
 
         L_EUV has units photons/s
@@ -182,7 +180,7 @@ class Parravano:
             if self.M < 7.:
                 return 2.23*1e34*(self.M**11.5) << u.ph/u.s
             elif self.M < 12:
-                return 3.69*1e36*(self.M**8.87) << u.ph/u.s        
+                return 3.69*1e36*(self.M**8.87) << u.ph/u.s
             elif self.M < 20:
                 return 4.8*1e38*(self.M**7.85) << u.ph/u.s
             elif self.M < 30:
@@ -194,22 +192,22 @@ class Parravano:
             elif self.M < 120:
                 return 2.39*1e46*(self.M**1.76) << u.ph/u.s
         else:
-            return np.nan    
-        
+            return np.nan
+
     def LFUV_LH2(self):
         """
-        Parravano et al. (2003) gives scalling relations for L_FUV-L_H2, 
+        Parravano et al. (2003) gives scalling relations for L_FUV-L_H2,
         which is basically the FUV band excluding the narrow "H_2 band".
-        
+
         ----
-        
-        L_FUV-L_H2 has units of Lsun. 
+
+        L_FUV-L_H2 has units of Lsun.
         """
         if self.M >= 1.8:
             if self.M < 2.:
                 return 2.77*1e-4*(self.M**11.8) << u.L_sun
             elif self.M < 2.5:
-                return 1.88*1e-3*(self.M**9.03) << u.L_sun 
+                return 1.88*1e-3*(self.M**9.03) << u.L_sun
             elif self.M < 3.:
                 return 1.19*1e-2*(self.M**7.03) << u.L_sun
             elif self.M < 6.:
@@ -223,18 +221,18 @@ class Parravano:
             elif self.M < 120.:
                 return 3.99*1e2*(self.M**1.54) << u.L_sun
         else:
-            return np.nan       
-        
+            return np.nan
+
     def LH2(self):
         """
         Gives the luminosity in the narrow "H_2 band"
         This covers about 10% of the FUV band in the wavelength range
-        912-1100\AA. 
-        
+        912-1100\AA.
+
         ----
-        
+
         L_H2 has units of Lsun
-        """       
+        """
         if self.M >= 1.8:
             if self.M < 3:
                 return 1.98*1e-14*(self.M**26.6) << u.L_sun
@@ -251,21 +249,21 @@ class Parravano:
             elif self.M < 30.:
                 return 9.07*(self.M**2.39) << u.L_sun
             elif self.M < 120.:
-                return 9.91*1e1*(self.M**1.69) << u.L_sun 
+                return 9.91*1e1*(self.M**1.69) << u.L_sun
         else:
             return np.nan
-        
+
     def LFUV(self):
         """
-        Gives the luminosity in the whole FUV band, which covers 
+        Gives the luminosity in the whole FUV band, which covers
         6eV<=h\nu<=13.4eV, or 919-2070\AA
-        
+
         ----
-        
+
         L_FUV is given in units erg/s (cgs)
-        """               
+        """
         return (self.LFUV_LH2() + self.LH2()).cgs
-  
+
     def localFlux(self,L, d):
         """
         Given any luminosity, return the flux at a distance d, in parsec
@@ -274,18 +272,18 @@ class Parravano:
             d = d << u.pc
         buffer = 1e-10*u.pc
         return L/4./np.pi/(d-buffer)**2
-    
+
     def localFUV(self, d):
         """
-        Return the Local FUV flux at a distance d (in parsec), in G0 units 
+        Return the Local FUV flux at a distance d (in parsec), in G0 units
         """
         return self.localFlux(self.LFUV(), d).to(self.G0)
-    
+
     def localEUV(self, d):
         """
-        Return the Local EUV flux at a distance d (in parsec), in  
+        Return the Local EUV flux at a distance d (in parsec), in
         photons/s/cm2
-        """        
-        return self.localFlux(self.LEUV(), 
+        """
+        return self.localFlux(self.LEUV(),
                               d).decompose(bases=[u.cm, u.ph, u.s])
-    
+
--- astroFIRE/StarEvolution.py
+++ astroFIRE/StarEvolution.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Mon Jul 12 14:17:00 2021
 
@@ -9,7 +7,7 @@ Contains functions for loading stellar evolution models that will be fed
 to the SpinEvolution.py code.
 
 Version of 13th July 2021:
-    
+
     Only the Baraffe et al (2015, BHAC15) model is currently available as
     BHAC15_MassTrack
 
@@ -21,32 +19,32 @@ from astropy import units as u
 class BHAC15_MassTrack:
     """
     Loads Baraffe et al. (2015, BHAC15) Mass Tracks
-    
+
     Tracks are for Solar Metalicity stars.
     source: http://perso.ens-lyon.fr/isabelle.baraffe/BHAC15dir/
-    
+
     ------------------------------------------------------------------------
     Version date 13th July 2021
-    
+
     Usage:
 
         To load the grid:
-            
-            grid = BHAC15_MassTrack() 
-        
+
+            grid = BHAC15_MassTrack()
+
         This will return the object:
-            
+
             grid.BHAC15
-            
+
         which contains the whole grid. Parameter available are detailed in [1]
         To get mass tracks for a specific mass (after loading the grid!):
-            
+
         grid.getMassTrack(mass)
-        
+
         this will load grid.Params for the basic Params:
-            
+
             grid.Mass = mass in Msun
-            grid.Teff = effective temperature in K 
+            grid.Teff = effective temperature in K
             grid.Age = age in years
             grid.R = Radius in Rsun
             grid.k2rad = radiative gyration radius
@@ -54,9 +52,9 @@ class BHAC15_MassTrack:
             grid.k2_2 = gyration radius - see note [2]
             grid.I = Momentum of Inertia (Isun) see note [2]
 
-        If full=True, it will load the remaining parameters available with 
+        If full=True, it will load the remaining parameters available with
         the BHAC15 grid:
-            
+
                 grid.Lum = Luminosity in Lsun
                 grid.logg = surface gravity (logg)
                 grid.log_Li = Li abundance
@@ -66,20 +64,20 @@ class BHAC15_MassTrack:
                 grid.Rrad =  radiative core in Rsun
 
     ------------------------------------------------------------------------
-    
+
     [1] Extracted from BHAC15 Notes:
-    BHAC15 tracks and internal structure for brown dwarfs and low mass star 
+    BHAC15 tracks and internal structure for brown dwarfs and low mass star
     (0.075 Msun to 1.4 Msun)
 
     M/Ms:  mass of the star in units of solar mass
     log t: age of the star (in yr)
     Teff: effective temperature (in K)
-    L/Ls: log luminosity in units of solar luminosity 
+    L/Ls: log luminosity in units of solar luminosity
           (value used Ls=3.839d+33)
     g: log g  (surface gravity)
-    R/Rs : radius of the star in units of solar radius    
+    R/Rs : radius of the star in units of solar radius
            (value used Rs=6.96d10)
-    log(Li/Li0): log of the ratio of surface lithium abundance to initial 
+    log(Li/Li0): log of the ratio of surface lithium abundance to initial
                  abundance
     log Tc : log of central temprature
     log ROc: log of central density (in gr/cc)
@@ -89,7 +87,7 @@ class BHAC15_MassTrack:
     k2rad: radiative gyration radius
 
     ------------------
-    
+
     [2] NOTE: Adopted convention to calculate the gyration radii:
 
     k2conv=[ I/(R**2 * Mstar) ]**(1/2)
@@ -107,20 +105,20 @@ class BHAC15_MassTrack:
     k2conv= 8.993E-02
     k2rad= 2.519E-01
 
-    k2**2 = k2conv**2 + k2rad**2 ~ 0.071 
+    k2**2 = k2conv**2 + k2rad**2 ~ 0.071
     (which is the typical value found for the Sun)
 
     and the moment of inertia is:
      ===> I = k2**2 * Mstar * R**2
 
     END OF NOTE
-    
+
     ------------------------------------------------------------------------
 
-    '*_res files are based on the original grid, but were resampled to 
+    '*_res files are based on the original grid, but were resampled to
     guarantee a regular timesampling across different mass tracks
-    
-    
+
+
     """
 
     def __init__(self, full=False):
@@ -129,26 +127,26 @@ class BHAC15_MassTrack:
                            'data/stellar_model/BHAC15/')
         self.M_o = 1.99e33 << u.g
         self.R_o = 6.96e10 << u.cm
-        self.I_o = 7e53 << u.g*(u.cm**2) 
-        self.masses = np.array([0.075, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 
+        self.I_o = 7e53 << u.g*(u.cm**2)
+        self.masses = np.array([0.075, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5,
                                 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3,1.4])
         self.BHAC15 = {}
-        for key in ['M_Ms', 'log_t_yr', 'Teff',  'R_Rs', 'I_Is', 'k2conv', 
+        for key in ['M_Ms', 'log_t_yr', 'Teff',  'R_Rs', 'I_Is', 'k2conv',
                     'k2rad']:
-            self.BHAC15[key] = [] 
+            self.BHAC15[key] = []
         if bool(full):
             for key in ['L_Ls', 'g', 'Log_Li_Li0', 'logTc', 'logROc', 'Mrad',
                          'Rrad']:
-                self.BHAC15[key] = [] 
+                self.BHAC15[key] = []
         for mass in self.masses:
-            filename = '{:5.3f}'.format(mass)[0] + 'p' 
+            filename = '{:5.3f}'.format(mass)[0] + 'p'
             filename += '{:5.3f}'.format(mass)[2:] + 'Msu_masstrack_res.txt'
-            data = Table.read(datadir + filename, format='ascii')   
-            for key in ['M_Ms', 'log_t_yr', 'Teff',  'R_Rs', 'k2conv', 
+            data = Table.read(datadir + filename, format='ascii')
+            for key in ['M_Ms', 'log_t_yr', 'Teff',  'R_Rs', 'k2conv',
                         'k2rad']:
                 self.BHAC15[key].extend(data[key])
             if bool(full):
-                for key in ['L_Ls', 'g', 'Log_Li_Li0', 'logTc', 'logROc', 
+                for key in ['L_Ls', 'g', 'Log_Li_Li0', 'logTc', 'logROc',
                             'Mrad', 'Rrad']:
                     self.BHAC15[key].extend(data[key])
             self.BHAC15['I_Is'].extend((data['k2conv']**2 + data['k2rad']**2)*
@@ -157,7 +155,7 @@ class BHAC15_MassTrack:
         for key in self.BHAC15.keys():
             self.BHAC15[key]=np.array(self.BHAC15[key])[valid]
         self.full = full
-            
+
     def getMassTrack(self, mass):
         if mass in self.masses:
             select_mass = (self.BHAC15['M_Ms'] == mass)
@@ -171,11 +169,11 @@ class BHAC15_MassTrack:
                 self.Tcentral = 10**self.BHAC15['logTc'][select_mass]
                 self.Rhocentral = 10**self.BHAC15['logROc'][select_mass]
                 self.Mrad = self.BHAC15['Mrad'][select_mass]
-                self.Rrad = self.BHAC15['Rrad'][select_mass]   
+                self.Rrad = self.BHAC15['Rrad'][select_mass]
                 self.Lum = self.BHAC15['L_Ls'][select_mass]
-                self.logg = self.BHAC15['g'][select_mass]  
+                self.logg = self.BHAC15['g'][select_mass]
                 self.k2conv = self.BHAC15['k2conv'][select_mass]
-                self.k2rad = self.BHAC15['k2rad'][select_mass]    
+                self.k2rad = self.BHAC15['k2rad'][select_mass]
                 self.k2_2 = self.BHAC15['k2rad']**2 + self.BHAC15['k2conv']**2
         elif (mass < 0.075) or (mass > 1.4):
              print('outside mass range')
@@ -191,20 +189,20 @@ class BHAC15_MassTrack:
             select_a = (self.BHAC15['M_Ms'] == mass_a)
             select_b = (self.BHAC15['M_Ms'] == mass_b)
             def interpolate(key, select_a = select_a, select_b = select_b):
-                return np.array([np.interp(mass, x, y) for y in 
-                                 list(zip(self.BHAC15[key][select_a], 
+                return np.array([np.interp(mass, x, y) for y in
+                                 list(zip(self.BHAC15[key][select_a],
                                           self.BHAC15[key][select_b]))])
             self.Mass = mass
             self.Teff = interpolate('Teff')
             self.Age = 10**interpolate('log_t_yr')
             self.R = interpolate('R_Rs')
             self.k2rad = interpolate('k2rad')
-            self.k2conv = interpolate('k2conv')     
+            self.k2conv = interpolate('k2conv')
             self.k2_2=self.k2conv**2+self.k2rad**2
-            self.I=self.k2_2*self.Mass*(self.R**2)            
-            if bool(self.full):                
+            self.I=self.k2_2*self.Mass*(self.R**2)
+            if bool(full):
                 self.Lum = interpolate('L_Ls')
-                self.logg = interpolate('g')         
+                self.logg = interpolate('g')
                 self.log_Li = interpolate('Log_Li_Li0')
                 self.Tcentral = 10**interpolate('logTc')
                 self.Rhocentral = 10**interpolate('logROc')
