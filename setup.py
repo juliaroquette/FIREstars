1278d494317a027b092eb652348125b873c8f8be
Julia Roquette <jt574@exeter.ac.uk>
2021-08-17 13:38:18 +0100

    Fixed missing comas
    Fixed missing comas

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     version='0.1',
     packages=find_packages(),    
     author = 'Julia Roquette',
-    author_email = 'juliaroquette@gmail.com'
-    license = "MIT"
-    url = 'https://github.com/juliaroquette/FIREstars'
+    author_email = 'juliaroquette@gmail.com',
+    license= = "MIT",
+    url = 'https://github.com/juliaroquette/FIREstars',
     description = 'Far-ultraviolet Irradiated Rotational Evolution model for low mass stars'
 )
