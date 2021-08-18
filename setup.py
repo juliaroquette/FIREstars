1de220ad8ad544a5bb6e415b06920fed2d7a964e
Julia Roquette <jt574@exeter.ac.uk>
2021-08-18 13:36:02 +0100

    Update setup.py
    Added author and contact info to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
+
+description = "Far-ultraviolet Irradiated Rotational Evolution"
+description += "model for low mass stars"
+
 setup(
     name='FIREstars',
     version='0.1',
-#    author='Julia Roquette',
-#    author_email= juliaroquette@gmail.com',
-#    license="MIT",
-#    url='https://github.com/juliaroquette/FIREstars',
-#    description='Far-ultraviolet Irradiated Rotational Evolution model for low mass stars',
+    url="https://github.com/juliaroquette/FIREstars",
+    author="Julia Roquette",
+    author_email="juliaroquette@gmail.com",
+    description=description,
     packages=find_packages(),
     include_package_data=True,
 )
